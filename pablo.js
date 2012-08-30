f80f1ec8e9638a8388fce128bd745d0452f74602
Premasagar Rose <p@dharmafly.com>
2012-08-30 16:57:41 +0100

    Add hyphenated element names as method names

--- pablo.js
+++ pablo.js
@@ -703,12 +703,12 @@ var Pablo = (function(document, Array, JSON, Element, NodeList){
     // SVG element methods
     'a altGlyph altGlyphDef altGlyphItem animate animateColor animateMotion animateTransform circle clipPath color-profile cursor defs desc ellipse feBlend feColorMatrix feComponentTransfer feComposite feConvolveMatrix feDiffuseLighting feDisplacementMap feDistantLight feFlood feFuncA feFuncB feFuncG feFuncR feGaussianBlur feImage feMerge feMergeNode feMorphology feOffset fePointLight feSpecularLighting feSpotLight feTile feTurbulence filter font font-face font-face-format font-face-name font-face-src font-face-uri foreignObject g glyph glyphRef hkern image line linearGradient marker mask metadata missing-glyph mpath path pattern polygon polyline radialGradient rect script set stop style svg switch symbol text textPath title tref tspan use view vkern'.split(' ')
         .forEach(function(nodeName){
-            var methodName = hyphenatedToCamelCase(nodeName);
+            var hyphenated = hyphenatedToCamelCase(nodeName);
             
-            Pablo[methodName] = function(attr){
+            Pablo[nodeName] = Pablo[hyphenated] = function(attr){
                 return Pablo(nodeName, attr || {});
             };
-            pabloNodeApi[methodName] = function(attr){
+            pabloNodeApi[nodeName] = pabloNodeApi[hyphenated] = function(attr){
                 return this.child(nodeName, attr || {});
             };
         });
