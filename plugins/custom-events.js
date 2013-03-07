56220aaab2e8c7f101dcab0f3ca4ca15582eb180
Premasagar Rose <p@dharmafly.com>
2013-03-07 00:59:26 +0000

    Add event delegation to custom-events plugin

--- plugins/custom-events.js
+++ plugins/custom-events.js
@@ -13,82 +13,151 @@
     	* Custom event types can be created
 
     	Note:
-    	`one()` and `oneEach()` are not modified in this extension but they
-    	anyway utilise the optional `context` argument in Pablo core
+    	`oneEach()` is not modified in this extension because it already
+        passes all its arguments through to `on()` and `off()`
     */
 
     // TODO: implement `.off()` with no args to remove all listeners
-    // TODO: implement delegate events (will need to change core one/oneEach too)
 
     var prefix = '__events__'; 
 
-    Pablo.fn.on = function(type, listener, useCapture, context){
-    	var wrapper, event;
+    Pablo.fn.on = function(type, selectors, listener, useCapture, context){
+    	var boundContext, wrapper, event;
 
-		if (context){
-			wrapper = function(){
-				listener.apply(context, arguments);
-			};
-		}
+        // `selectors` argument not given
+        if (typeof selectors === 'function'){
+            context = useCapture;
+            useCapture = listener;
+            listener = selectors;
+            selectors = null;
+        }
+
+        if (typeof useCapture === 'undefined'){
+            useCapture = false;
+        }
+
+        wrapper = listener;
+
+        // If a `this` object is given, then the listener is bound to a wrapper 
+        // function that uses the required `this` context
+        if (context){
+            boundContext = function(){
+                listener.apply(context, arguments);
+            };
+            wrapper = boundContext;
+        }
+
+        // Prepare data to be cached about the event
 		event = {
+            selectors:  selectors,
 			listener:   listener,
 			wrapper:    wrapper,
-			useCapture: useCapture || false
+			useCapture: useCapture
 		};
 
+        // If there are multiple, space-delimited event type, then cycle through
+        // each one
 		return this.processList(type, function(type){
 			var key = prefix + type;
 
+            // Cycle through each element in the collection
 			this.each(function(el){
         		var node = Pablo(el),
+                    // Retrieve the element's cached event data
         			eventCache = node.data(key);
 
+                // Create a container for the cached event data
 	        	if (!eventCache){
 	        		eventCache = [];
 	        		node.data(key, eventCache);
 	        	}
 
+                // Delegate events
+                if (selectors){
+                    wrapper = event.wrapper = function(event){
+                        var target = event && event.target;
+                        if (target){
+                            if (Pablo(target).is(selectors, node)){
+                                (boundContext || listener).apply(null, arguments);
+                            }
+                        }
+                    };
+                }
+
 	        	// Add to cache
 				eventCache.push(event);
 
 				// Add DOM listener
-				el.addEventListener(type, wrapper || listener, useCapture);
+				el.addEventListener(type, wrapper, useCapture);
         	});
 		});
     };
 
-    Pablo.fn.off = function(type, listener, useCapture){
+    Pablo.fn.off = function(type, selectors, listener, useCapture){
+        // `selectors` argument not given
+        if (typeof selectors === 'function'){
+            context = useCapture;
+            useCapture = listener;
+            listener = selectors;
+            selectors = null;
+        }
+
+        if (typeof useCapture === 'undefined'){
+            useCapture = false;
+        }
+
     	return this.processList(type, function(type){
     		var key = prefix + type;
 
-    		if (typeof useCapture === 'undefined'){
-    			useCapture = false;
-    		}
-
     		this.each(function(el){
     			var node = Pablo(el),
-    				eventCache = this.data(key);
+    				eventCache = node.data(key);
 
     			if (eventCache){
 	        		// Use `some` rather than `forEach` to allow breaking 
 	        		// the loop. Use `some` rather than `for` as it's a 
 	        		// sparse array.
-	        		eventCache.some(function(event, id){
-	        			var wrapper;
-	        			if (event.listener === listener && event.useCapture === useCapture){
-	        				wrapper = event.wrapper;
-
+	        		eventCache.some(function(event, i){
+	        			if (listener === event.listener &&
+                            useCapture === event.useCapture &&
+                            selectors === event.selectors
+                        ){
 	        				// Remove from cache
-	        				delete eventCache[id];
+	        				delete eventCache[i];
 
 	        				// Remove DOM listener
-							el.removeEventListener(type, wrapper || listener, useCapture);
+							el.removeEventListener(type, event.wrapper, useCapture);
 	        				return true;
 	        			}
 	        		}, this);
 	        	}
     		});
     	});
+    };
+
+    Pablo.fn.one = function(type, selectors, listener, useCapture, context){
+        var collection = this;
+
+        // `selectors` argument not given
+        if (typeof selectors === 'function'){
+            context = useCapture;
+            useCapture = listener;
+            listener = selectors;
+            selectors = null;
+        }
+
+        function removeListener(){
+            // Remove listener and additional listener
+            collection.off(type, selectors, listener,       useCapture, context)
+                      .off(type, selectors, removeListener, useCapture, context);
+        }
+
+        // Add the original listener, and an additional listener that removes
+        // the first, and itself. The reason a wrapper listener is not used
+        // instead of two separate listeners is to allow manual removal of
+        // the original listener (with `.off()`) before it ever triggers.
+        return this.on(type, selectors, listener,       useCapture, context)
+                   .on(type, selectors, removeListener, useCapture, context);
     };
 
     Pablo.fn.trigger = function(type){
