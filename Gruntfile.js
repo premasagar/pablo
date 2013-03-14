c44b48e4040182dc64100c0390f3861ea5ba57c4
aaron <aaron@crunchbang.(none)>
2013-03-14 15:11:17 +0000

    Add grunt file

---
+++ Gruntfile.js
@@ -0,0 +1,35 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    uglify: {
+      options: {
+        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+      },
+      build: {
+        src: '<%= pkg.name %>.js',
+        dest: 'build/<%= pkg.name %>.min.js'
+      }
+    },
+    jshint: {
+      src: '<%= pkg.name %>.js',
+      options: {
+        curly: true
+      },
+      globals: {
+        document: true,
+        window: true
+      }
+    }
+  });
+
+  // Load the plugin that provides the "jshint" task.
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  // Load the plugin that provides the "uglify" task.
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+
+  // Default task(s).
+  grunt.registerTask('default', ['jshint', 'uglify']);
+
+};
