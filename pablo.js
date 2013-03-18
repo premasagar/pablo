premasagar/pablo

commit c5d61afd3f79faf4144fb3b8c233b15bc06fbbdc

Rename Pablo.select() to Pablo.find(), and make more efficient

pablo.js
@@ -150,7 +150,7 @@
         if (Pablo.isPablo(node)){
             return attr ? node.attr(attr) : node;
         }
-        return Pablo(node, attr);
+        return Pablo.create(node, attr);
     }
 
     // Return CSS styles with browser vendor prefixes
@@ -1096,29 +1096,29 @@
 
     // PABLO API
 
-    // Create Pablo: return a PabloCollection instance
-    function createPablo(node, attr){
-        return new PabloCollection(node, attr);
-    }
-    
-    // Select existing nodes in the document
-    function selectPablo(selectors, context){
-        return toPablo(context || document).find(selectors);
-    }
-
-    
-    // **
-    
-
     // Pablo main function
     function Pablo(node, attr){
+        // TODO: if `node` is a string and `attr` is an object, but not a plain object, then use it as a context and pass it with `node` to `find()`
         if (!node || attr || Pablo.canBeWrapped(node)){
             return Pablo.create(node, attr);
         }
         else {
-            return Pablo.select(node);
+            return Pablo.find(node);
         }
     }
+
+    // Create a Pablo collection
+    Pablo.create = function(node, attr){
+        return new PabloCollection(node, attr);
+    };
+    
+    // Select existing nodes
+    Pablo.find = function(selectors, context){
+        if (!context){
+            return Pablo.create(document.querySelectorAll(selectors));
+        }
+        return Pablo.create(context).find(selectors);
+    };
     
     // Pablo methods
     extend(Pablo, {
@@ -1134,8 +1134,6 @@
 
         // methods
         make: make,
-        create: createPablo,
-        select: selectPablo,
         isArrayLike: isArrayLike,
         isElement: isElement,
         isSVGElement: isSVGElement,

commit 50cb91a5f9dd1c246cefd2e733cd449f55cfa647

Fix toPablo() so that it doesn't create elements when a selector is passed - regression from c5d61afd3f79faf4144fb3b8c233b15bc06fbbdc

pablo.js
@@ -48,6 +48,7 @@
         testElement && head && arrayProto && 
         Element && SVGElement && NodeList && HTMLDocument && 
         'createSVGRect' in testElement &&
+        'attributes' in testElement &&
         'querySelectorAll' in document &&
         'previousElementSibling' in head &&
         'children' in head &&
@@ -134,6 +135,7 @@
         return obj instanceof HTMLDocument;
     }
 
+    // Check if obj is an element from this or another document
     function hasSvgNamespace(obj){
         return obj && obj.namespaceURI && obj.namespaceURI === svgns;
     }
@@ -152,12 +154,12 @@
             hasSvgNamespace(obj);
     }
     
-    // Return node (with attributes) if a Pablo collection, otherwise create one
+    // Return node (with attributes) if a Pablo collection, otherwise create one.
     function toPablo(node, attr){
         if (Pablo.isPablo(node)){
             return attr ? node.attr(attr) : node;
         }
-        return Pablo.create(node, attr);
+        return Pablo(node, attr);
     }
 
     // Return CSS styles with browser vendor prefixes